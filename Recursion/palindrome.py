36de59bbd9ceec8d9250d82d5d9a2bf40fc609d6
oftedahld <oftedahld@my.lanecc.edu>
2022-04-17 21:03:35 -0700

    Completed the palindrome problem

--- Recursion/palindrome.py
+++ Recursion/palindrome.py
@@ -1,5 +1,32 @@
 def main():
-    print("palindrome")
+    print("This program will allow a user to provide a string and determine if it's a palindrome...")
+    orgTestString = str(input("Provide a string to test: "))
+    #testString = "Teet"
+    testString = scrubber(orgTestString)
+    isPalindrome = palindrome(testString)
+    if isPalindrome == True:
+        print('String "' + (orgTestString) + '" ' + 'IS a palindrome!')
+    else:
+        print('String "' + (orgTestString) + '" ' + 'IS NOT a palindrome!')
 
+def palindrome(testString):
+    stringLength = int(len(testString))
+    if stringLength >= 2:
+        if testString[0] == testString[stringLength - 1]:
+            testString = testString[1:stringLength-1]
+            return palindrome(testString)
+        else:
+            return False
+    else:
+        return True
+
+def scrubber(string):
+    string = string.lower()
+    stringList = list(string)
+    modStringList = ""
+    for i in stringList:
+        if i.isalnum():
+            modStringList += i
+    return modStringList
 
 main()
